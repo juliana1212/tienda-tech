0e08ae532cce3a09db59c7bbc5afcced13a079b1
Nataliafg7 <nataliaflorezz7282@gmail.com>
2025-10-10 15:04:31 -0500

    módulos categories y products

--- src/app.module.js
+++ src/app.module.js
@@ -1,5 +1,7 @@
 "use strict";
 import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
